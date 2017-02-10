11f4847aadcd07c388a664eba268e6c938d65c9d
Ninio (Nikolay Ninarski) <nikolay.ninarski@gmail.com>
2017-02-10 16:37:14 +0200

    Fixes #1 - Triggering event on an instance calls other instances callbacks also

--- eventy.js
+++ eventy.js
@@ -13,17 +13,14 @@
 		 * @return {[type]}
 		 */
 		eventEnable: function ( NewEventEnabledObject ) {
+			NewEventEnabledObject._eventHandlers = {};
 			/**
 			 * addEventListener an event for NewEventEnabledObject
 			 * @param  {String} eventName  The name of the event
 			 * @param  {Function} handler  The callback
 			 */
 			NewEventEnabledObject.addEventListener = function ( eventName, handler ) {
-				if( NewEventEnabledObject._eventHandlers === undefined ) {
-					NewEventEnabledObject._eventHandlers = {};
-				}
-
-				if( NewEventEnabledObject._eventHandlers[ eventName ] === undefined ) {
+				if(  NewEventEnabledObject._eventHandlers[ eventName ] === undefined ) {
 					NewEventEnabledObject._eventHandlers[ eventName ] = [];
 				}
 
@@ -33,7 +30,7 @@
 				else {
 					console.error( 'Cannot attach the handler - it is not a function.');
 				}
-			}
+			};
 
 			/**
 			 * triggerEvent an event for a NewEventEnabledObject
@@ -54,7 +51,7 @@
 				}
 
 				return isEventSuccessful;
-			}
+			};
 
 			/**
 			 * removeEventListener event handler
@@ -75,21 +72,76 @@
 					}
 				}
 				return null;
-			}
+			};
 
 
 			// Enable instance-invoked events only if there are instances
 			if( NewEventEnabledObject.prototype ) {
-				NewEventEnabledObject.prototype.addEventListener = function( eventName, handler ) {
-					NewEventEnabledObject.addEventListener.call( this, eventName, handler );
+
+				/**
+				 * addEventListener an event for NewEventEnabledObject
+				 * @param  {String} eventName  The name of the event
+				 * @param  {Function} handler  The callback
+				 */
+				NewEventEnabledObject.prototype.addEventListener = function ( eventName, handler ) {
+					if( !this._eventHandlers ) {
+						this._eventHandlers = {};
+					}
+
+					if( this._eventHandlers[ eventName ] === undefined ) {
+						this._eventHandlers[ eventName ] = [];
+					}
+
+					if( typeof handler === 'function' ) {
+						this._eventHandlers[ eventName ].push( handler );
+					}
+					else {
+						console.error( 'Cannot attach the handler - it is not a function.');
+					}
 				};
 
-				NewEventEnabledObject.prototype.triggerEvent = function( eventName, options ) {
-					NewEventEnabledObject.triggerEvent.call( this, eventName, options );
+				/**
+				 * triggerEvent an event for a NewEventEnabledObject
+				 * @param  {String} eventName  The name of the event
+				 * @param  {Object} options    The single argument for the callback. Can consist of anything
+				 */
+				NewEventEnabledObject.prototype.triggerEvent = function ( eventName, options ) {
+					var isEventSuccessful = true;
+
+					if( this._eventHandlers !== undefined ) {
+						if( this._eventHandlers[ eventName ] !== undefined ) {
+							for( var handlerIndex = 0; handlerIndex < this._eventHandlers[ eventName ].length; handlerIndex++ ) {
+								if( typeof this._eventHandlers[ eventName ][ handlerIndex ] === 'function' ) {
+									isEventSuccessful = ( this._eventHandlers[ eventName ][ handlerIndex ].call( this, options ) !== false ) && isEventSuccessful;
+								}
+							}
+						}
+					}
+
+					isEventSuccessful = ( NewEventEnabledObject.triggerEvent.call( this, eventName, options ) !== false ) && isEventSuccessful;
+
+					return isEventSuccessful;
 				};
 
-				NewEventEnabledObject.prototype.removeEventListener = function( eventName, handler ) {
-					NewEventEnabledObject.removeEventListener.call( this, eventName, handler );
+				/**
+				 * removeEventListener event handler
+				 * @param  {String} eventName  The name of the event
+				 * @param  {Function} handler  The handler that we want to remove
+				 * @return {Function|null}     The removed handler
+				 */
+				NewEventEnabledObject.prototype.removeEventListener = function ( eventName, handler ) {
+					if( typeof handler === 'function' ) {
+						if( this._eventHandlers !== undefined ) {
+							if( this._eventHandlers[ eventName ] !== undefined ) {
+								for( var handlerIndex = 0; handlerIndex < this._eventHandlers[ eventName ].length; handlerIndex++ ) {
+									if( this._eventHandlers[ eventName ][ handlerIndex ] === handler ) {
+										return this._eventHandlers[ eventName ].splice( handlerIndex, 1 );
+									}
+								}
+							}
+						}
+					}
+					return null;
 				};
 			}
 		}
